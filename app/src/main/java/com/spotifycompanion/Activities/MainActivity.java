f286c376d08c5dcc2d4e666ac72da3e40ebb301c
MD5-9029 <64097452+MD5-9029@users.noreply.github.com>
2021-01-04 05:19:11 +0100

    removed obsolete activities

--- app/src/main/java/com/spotifycompanion/Activities/MainActivity.java
+++ app/src/main/java/com/spotifycompanion/Activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.spotifycompanion.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -58,12 +57,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnAuth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAuthActivity();
-            }
-        });
 
         btnBottomMiddle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,8 +66,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void openAuthActivity() {
-        Intent intent = new Intent(MainActivity.this, TestActivity.class);
-        startActivity(intent);
-    }
 }
