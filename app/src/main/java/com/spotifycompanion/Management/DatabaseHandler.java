4bfec3de3bc7929b8df412f1812f7687513983e6
MD5-9029 <64097452+MD5-9029@users.noreply.github.com>
2021-01-11 16:54:05 +0100

    basic methods for SQlite

--- app/src/main/java/com/spotifycompanion/Management/DatabaseHandler.java
+++ app/src/main/java/com/spotifycompanion/Management/DatabaseHandler.java
@@ -1,13 +1,15 @@
 package com.spotifycompanion.Management;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static String gDatabase = "companion.db";
+    private static final String gDatabase = "companion.db";
+    private static final String gTable = "skipped";
 
     public DatabaseHandler(@Nullable Context pContext) {
         super(pContext, gDatabase, null, 1);
@@ -16,13 +18,30 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase pDB) {
-        String lCreateTableString = "CREATE TABLE skipped (id int PRIMARY KEY AUTOINCREMENT)";
+        String lCreateTableString = "CREATE TABLE skipped (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "uri TEXT NOT NULL)";
         pDB.execSQL(lCreateTableString);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addSkipped(String pUri) {
+        SQLiteDatabase lDb = this.getWritableDatabase();
+        lDb.execSQL("INSERT INTO " + gTable + "(uri) VALUES (" + pUri + ")");
+        lDb.close();
+    }
+
+    public int getSkipped(String pUri) {
+        SQLiteDatabase lDb = this.getReadableDatabase();
+        Cursor lC = lDb.rawQuery("SELECT COUNT * FROM" + gTable + "WHERE uri = " + pUri + ";", null);
+        if (lC.moveToFirst())
+            return lC.getInt(0);
+        else
+            return -1;
     }
 
 }
