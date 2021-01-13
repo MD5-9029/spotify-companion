dcc3e7c575b924a53d4aa41c50f84d89d0c490e1
Dominik <11911481+Deltachi@users.noreply.github.com>
2021-01-13 04:07:38 +0100

    Purge of the old connectivity code, integration of Manager functionality, DISABLED STRICT MODE
    Login, Logoff, AuthCheck, DataRequests now all go through the Manager.
    The ThreadPolicy -> StrictMode forced the app to crash, when a web request is startet synchronous insite the main-thread. Disabled that mode to prevent "safety-crashes"

--- app/src/main/java/com/spotifycompanion/Activities/MainActivity.java
+++ app/src/main/java/com/spotifycompanion/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.spotifycompanion.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -18,6 +19,9 @@ import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
 import com.spotifycompanion.Management.ManagementConnector;
 import com.spotifycompanion.R;
+import com.spotifycompanion.models.Playlist;
+import com.spotifycompanion.models.Playlists;
+import com.spotifycompanion.models.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,6 +35,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
     private final ManagementConnector gManagementConnector = new ManagementConnector(MainActivity.this);
     Button btBottomLeft, btBottomRight, btLogInOut, btBottomMiddle;
     Toolbar tbTop;
@@ -46,8 +52,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        requestToken();
+
+        //allow synchronous web calls in main thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         gManagementConnector.initialize();
+        gManagementConnector.authorizeAccess(this);
     }
 
     @Override
@@ -96,105 +107,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void rest(View view) {
         Button lBt = findViewById(R.id.bt_logInOut);
-        if(gManagementConnector.isAuthorized()) {
-            cancelCall();
-            onClearCredentialsClicked();
+        Log.e("UI", "Button clicked");
+        //Playlists lists = gManagementConnector.gRESTHandler.getUserPlaylists();
+        //Playlist list = gManagementConnector.gRESTHandler.getPlaylist(lists.items[3].id);
 
+        if(gManagementConnector.isAuthorized()) {
+            //logout
+            gManagementConnector.disallowAccess(this);
             lBt.setText(R.string.drawer_logIn);
         }else{
-            getUserProfile();
-
-            lBt.setText(R.string.drawer_logOut);
+            //login
+            gManagementConnector.authorizeAccess(this);
+            //lBt.setText(R.string.drawer_logOut); //button text change on successful login (onActivityResult)
         }
-    }
-
-
-    public static final String CLIENT_ID = "4234dd4558284817abdb7c7ecc4d7df7";
-    public static final String REDIRECT_URI = "spotifyCompanion://authCall";
-    public static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
-    public static final int AUTH_CODE_REQUEST_CODE = 0x11;
-
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
-    private String mAccessToken;
-    private String mAccessCode;
-    private Call mCall;
-
-
-    private Uri getRedirectUri() {
-        return Uri.parse(REDIRECT_URI);
-    }
-
-    public void cancelCall() {
-        if (mCall != null) {
-            mCall.cancel();
-        }
-    }
-
-    private AuthorizationRequest getAuthenticationRequest(AuthorizationResponse.Type type) {
-        return new AuthorizationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
-                .setShowDialog(false)
-                .setScopes(new String[]{"user-read-email"})
-                .setCampaign("your-campaign-token")
-                .build();
-    }
-
-    public void requestToken() {
-        final AuthorizationRequest request = getAuthenticationRequest(AuthorizationResponse.Type.TOKEN);
-        AuthorizationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request);
-    }
-
-    public void requestCode() {
-        final AuthorizationRequest request = getAuthenticationRequest(AuthorizationResponse.Type.CODE);
-        AuthorizationClient.openLoginActivity(this, AUTH_CODE_REQUEST_CODE, request);
     }
 
     public void onClearCredentialsClicked() {
-        AuthorizationClient.clearCookies(this);
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        final AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, data);
-
-        if (AUTH_TOKEN_REQUEST_CODE == requestCode) {
-            mAccessToken = response.getAccessToken();
-
-        } else if (AUTH_CODE_REQUEST_CODE == requestCode) {
-            mAccessCode = response.getCode();
-
+        Button lBt = findViewById(R.id.bt_logInOut);
+        if (requestCode == gManagementConnector.AUTH_TOKEN_REQUEST_CODE) {
+            boolean hasAccess = gManagementConnector.authorizeCallback(requestCode, resultCode, data);
+            if (hasAccess)lBt.setText(R.string.drawer_logOut);
         }
     }
-
-
-    public void getUserProfile() {
-        if (mAccessToken == null) {
-            return;
-        }
-
-        final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me")
-                .addHeader("Authorization", "Bearer " + mAccessToken)
-                .build();
-
-        cancelCall();
-        mCall = mOkHttpClient.newCall(request);
-
-        mCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                //setResponse("Failed to fetch data: " + e);
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    final JSONObject jsonObject = new JSONObject(response.body().string());
-                    Log.e("callback", jsonObject.toString());
-                } catch (JSONException e) {
-                    //setResponse("Failed to parse data: " + e);
-                }
-            }
-        });
-    }
-
 }
